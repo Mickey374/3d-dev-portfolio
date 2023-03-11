03dc8f79df755372136a7fb98c7e073f9cf59674
Mickey374 <asantemichael37483@gmail.com>
2023-03-11 19:37:24 +0100

    Added support for mobile screen rendering of max-width 500px.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,12 +16,28 @@ const Hero = () => {
             <span className="text-[#915eff]"> Michael</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I bring Products, 3D Visuals, User Experiences <br className="sm:block hidden"/> & Web Apps from Conception to Reality 
+            I bring Products, 3D Visuals, User Experiences <br className="sm:block hidden" /> & Web Apps from Conception to Reality
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
